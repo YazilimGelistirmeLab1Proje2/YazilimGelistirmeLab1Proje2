84fea681425fab030cba1ac8ffdac525e783c884
Fatih Bilgin <bilginfatih311@gmail.com>
2025-12-24 20:11:33 +0300

    BFS algoritmasi UserNode ve Edge yapisina gore tamamlandi

--- SocialNetwork/Algorithms/BFS.cs
+++ SocialNetwork/Algorithms/BFS.cs
@@ -12,32 +12,29 @@ namespace SocialNetwork.Algorithms
         public List<UserNode> BFSExecute(UserNode startNode)
         {
             Queue<UserNode> queue = new Queue<UserNode>();
-
             HashSet<UserNode> visited = new HashSet<UserNode>();
-
             List<UserNode> result = new List<UserNode>();
-
 
             if (startNode == null)
                 return result;
 
+           
             queue.Enqueue(startNode);
             visited.Add(startNode); 
 
             while (queue.Count > 0)
             {
+                
                 UserNode currentUser = queue.Dequeue();
 
                 result.Add(currentUser);
-                
+
                 if (currentUser.OutgoingEdges != null)
                 {
                     foreach (var edge in currentUser.OutgoingEdges)
                     {
-                       
                         UserNode neighbor = edge.Target;
 
-                     
                         if (!visited.Contains(neighbor))
                         {
                             visited.Add(neighbor);  
@@ -46,7 +43,12 @@ namespace SocialNetwork.Algorithms
                     }
                 }
             }
+
             return result;
+        }
+        public string GetResult()
+        {
+            return "BFS işlemi tamamlandı.";
         }
     }
 }
