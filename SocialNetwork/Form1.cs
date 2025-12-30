bb7c07165d15400dbd7538aee7eb94ed5bc67df5
efe <efeaydnfb@gmail.com>
2025-12-30 19:29:02 +0300

    bağlantı ekleme eklendi

--- SocialNetwork/Form1.cs
+++ SocialNetwork/Form1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -15,6 +16,7 @@ namespace SocialNetwork
         {
         }
 
+        // MATRİS RAPORU BUTONU
         private void btnMatrix_Click(object sender, EventArgs e)
         {
             if (graph == null) return;
@@ -36,6 +38,65 @@ namespace SocialNetwork
             }
 
             MessageBox.Show(rapor, "Ağ Raporu");
+        }
+
+        // BAĞLANTI EKLE BUTONU
+        private void btnEdgeEkle_Click(object sender, EventArgs e)
+        {
+            if (graph == null) return;
+
+            // Kutulardaki ID'leri al
+            if (int.TryParse(txtStart.Text, out int id1) && int.TryParse(txtEnd.Text, out int id2))
+            {
+                if (id1 == id2) 
+                { 
+                    MessageBox.Show("Kişi kendine bağlanamaz!"); 
+                    return; 
+                }
+
+                if (graph.Nodes.ContainsKey(id1) && graph.Nodes.ContainsKey(id2))
+                {
+                    UserNode u1 = graph.Nodes[id1];
+                    UserNode u2 = graph.Nodes[id2];
+
+                    // Zaten bağlılar mı kontrolü
+                    bool alreadyConnected = u1.OutgoingEdges.Any(edge => edge.Target.Id == id2);
+                    if (alreadyConnected)
+                    {
+                        MessageBox.Show("Bu iki kişi zaten bağlı!");
+                        return;
+                    }
+
+                    // AĞIRLIK HESAPLA (Öklid Uzaklığı Formülü)
+                    double diffActive = Math.Pow(u1.ActiveScore - u2.ActiveScore, 2);
+                    double diffInteract = Math.Pow(u1.InteractionCount - u2.InteractionCount, 2);
+                    double diffConnect = Math.Pow(u1.ConnectionCount - u2.ConnectionCount, 2);
+
+                    double euclidean = Math.Sqrt(diffActive + diffInteract + diffConnect);
+                    double weight = 1.0 / (1.0 + euclidean);
+
+                   
+                    Edge edge1 = new Edge { Source = u1, Target = u2, Weight = weight };
+                    u1.OutgoingEdges.Add(edge1);
+                    graph.Edges.Add(edge1);
+
+                    Edge edge2 = new Edge { Source = u2, Target = u1, Weight = weight };
+                    u2.OutgoingEdges.Add(edge2);
+                    graph.Edges.Add(edge2);
+
+                
+                    pnlGraph.Invalidate();
+                    MessageBox.Show($"Bağlantı kuruldu!\nHesaplanan Ağırlık: {weight:F4}");
+                }
+                else
+                {
+                    MessageBox.Show("Girilen ID'lerden biri veya ikisi bulunamadı.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Start ve End kutularına bağlanacak ID'leri yazın.");
+            }
         }
     }
 }
