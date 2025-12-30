24103513400b005889d3659e091dcdc82b6cc4c4
efe <efeaydnfb@gmail.com>
2025-12-30 19:24:04 +0300

    matris raporu için komşuluk listesi oluşturma işlevi eklendi

--- SocialNetwork/Form1.cs
+++ SocialNetwork/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace SocialNetwork
 {
     public partial class Form1 : Form
@@ -9,7 +13,29 @@ namespace SocialNetwork
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        }
 
+        private void btnMatrix_Click(object sender, EventArgs e)
+        {
+            if (graph == null) return;
+
+            string rapor = "--- KOMŞULUK LİSTESİ ---\n";
+
+            foreach (var node in graph.Nodes.Values)
+            {
+                rapor += $"[{node.Id} - {node.UserName}] -> ";
+
+                var neighborNames = node.OutgoingEdges.Select(edge => edge.Target.UserName).ToList();
+
+                if (neighborNames.Count > 0)
+                    rapor += string.Join(", ", neighborNames);
+                else
+                    rapor += "(Yalnız)";
+
+                rapor += "\n";
+            }
+
+            MessageBox.Show(rapor, "Ağ Raporu");
         }
     }
 }
